c55eb37e413915f0fabfdee9071916a159cbaf75
Victor Martins <victorcriciuma@gmail.com>
2026-01-10 22:41:28 -0300

    adicionado a rota de listagem filmes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-  res.send("Home page");
+app.get("/movies", async (req, res) => {
+  const movies = await prisma.movies.findMany();
+  res.json(movies);
 });
 
 app.listen(port, () => {
